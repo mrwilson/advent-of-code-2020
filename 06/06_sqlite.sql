943e466441d72ca91d0d359d254487a697e5c612
Alex Wilson <gpg@probablyfine.co.uk>
2020-12-27 16:11:02 +0000

    refactor(sql): Clean up entry for Day 6

--- 06/06_sqlite.sql
+++ 06/06_sqlite.sql
@@ -1,8 +1,6 @@
-DROP TABLE IF EXISTS customs_declarations;
+.load ../utils/split.dylib
 
-CREATE TABLE customs_declarations(
-  answer TEXT
-);
+create table customs_declarations(answer TEXT);
 
 .mode csv
 .import 06_input.txt customs_declarations
@@ -11,37 +9,25 @@ CREATE TABLE customs_declarations(
 -- passengers questions into a separate row so that we
 -- notionally have a table of (group, passenger, answer)
 
-create temporary table answers(answer, low, high, row);
-
-with passenger_groups(high, low) AS (
+create table answers as with passenger_groups(high, low) as (
     select
 	    rowid, lag(rowid,1,0) over (order by rowid asc) as next
     from
         customs_declarations where answer = ""
-),
-string_to_characters(row, label, str) AS (
-    select
-        rowid, '', answer
-    from
-        customs_declarations where answer != ""
-
-    union all
-
-    select
-        row, substr(str, 0, 2), substr(str, 2)
-    from
-        string_to_characters where str != ""
 )
-
-insert into answers
-    select
-        label, low, high, row
-    from
-        passenger_groups, string_to_characters
-    where
-        label != "" and row between low+1 and high-1
-    order by
-        low, row;
+select
+    split.value answer,
+    low,
+    high,
+    customs_declarations.rowid row
+from
+    passenger_groups,
+    customs_declarations,
+    split(customs_declarations.answer,"")
+where
+    customs_declarations.rowid between low+1 and high-1
+order by
+    2, 4;
 
 -- Part 1
 
