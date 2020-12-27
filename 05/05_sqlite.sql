0fa5b1cf7296a70c9663287f6bfc6e10b5be9367
Alex Wilson <gpg@probablyfine.co.uk>
2020-12-27 16:04:26 +0000

    refactor(sql): Clean up entry for Day 5

--- 05/05_sqlite.sql
+++ 05/05_sqlite.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS boarding_passes;
-
-CREATE TABLE boarding_passes (
-	code TEXT,
-	row INT,
-	seat INT,
-	seat_id INT GENERATED ALWAYS AS (row * 8 + seat) STORED
+create table boarding_passes (
+	code text,
+	row int,
+	seat int,
+	seat_id int generated always as (row * 8 + seat) stored
 );
 
 
@@ -12,32 +10,32 @@ CREATE TABLE boarding_passes (
 -- Generate all pairs (0..127,0..8) and derive their boarding pass number
 -- This is our precompute table so the problem-solving becomes "just" a lookup
 
-INSERT INTO boarding_passes WITH RECURSIVE
-row_number(row) AS (
-    SELECT 0 UNION ALL SELECT row+1 FROM row_number LIMIT 128
-), seat_number(seat) AS (
-     SELECT 0 UNION ALL SELECT seat+1 FROM seat_number LIMIT 8
- ) SELECT
+insert into boarding_passes with
+row_number(row) as ( select 0 union all select row+1 from row_number limit 128 ),
+seat_number(seat) as ( select 0 union all select seat+1 from seat_number LIMIT 8 )
+
+select
     -- Row number
-    CASE WHEN row & 64 THEN "B" ELSE "F" END
-    || CASE WHEN row & 32 THEN "B" ELSE "F" END
-    || CASE WHEN row & 16 THEN "B" ELSE "F" END
-    || CASE WHEN row & 8 THEN "B" ELSE "F" END
-    || CASE WHEN row & 4 THEN "B" ELSE "F" END
-    || CASE WHEN row & 2 THEN "B" ELSE "F" END
-    || CASE WHEN row % 2 THEN "B" ELSE "F" END
+    case when row & 64 then "B" else "F" end
+    || case when row & 32 then "B" else "F" end
+    || case when row & 16 then "B" else "F" end
+    || case when row & 8 then "B" else "F" end
+    || case when row & 4 then "B" else "F" end
+    || case when row & 2 then "B" else "F" end
+    || case when row % 2 then "B" else "F" end
     -- Seat number
-    || CASE WHEN seat & 4 THEN "R" ELSE "L" END
-    || CASE WHEN seat & 2 THEN "R" ELSE "L" END
-    || CASE WHEN seat % 2 THEN "R" ELSE "L" END,
+    || case when seat & 4 then "R" else "L" end
+    || case when seat & 2 then "R" else "L" end
+    || case when seat % 2 then "R" else "L" end,
     row,
     seat
-    FROM row_number, seat_number;
+    from
+        row_number,
+        seat_number;
 
 -- Load data
 
-DROP TABLE IF EXISTS test_passes;
-CREATE TABLE test_passes(code TEXT);
+create table test_passes(code TEXT);
 
 .mode csv
 .import 05_input.txt test_passes
@@ -50,22 +48,27 @@ select max(seat_id)
 
 -- Part 2
 
-create temporary table test_seat_ids as select
-    bp.seat_id from
+create temporary table test_seat_ids as
+    select
+        bp.seat_id
+    from
         boarding_passes bp,
-        test_passes tp where tp.code = bp.code;
-
-select seat_id
-    from boarding_passes
+        test_passes tp
     where
-        row not in (0, 127)
-        and
-        seat_id not in (select * from test_seat_ids)
-        and
-        seat_id - 1 in (select * from test_seat_ids)
-        and
-        seat_id + 1 in (select * from test_seat_ids)
-        ;
+        tp.code = bp.code;
+
+select
+    seat_id
+from
+    boarding_passes
+where
+    row not in (0, 127)
+    and
+    seat_id not in (select * from test_seat_ids)
+    and
+    seat_id - 1 in (select * from test_seat_ids)
+    and
+    seat_id + 1 in (select * from test_seat_ids);
 
 
 
