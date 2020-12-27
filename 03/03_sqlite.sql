544e24b9b07e361b8080af8481a65ae9405f86d0
Alex Wilson <gpg@probablyfine.co.uk>
2020-12-27 15:54:16 +0000

    refactor(sql): Redo Day 3 using split, include actual input

--- 03/03_sqlite.sql
+++ 03/03_sqlite.sql
@@ -1,17 +1,35 @@
--- Preprocess input into CSV of coordinates
-CREATE TABLE trees(x int, y int);
+.load ../utils/split.dylib
 
--- 31 is the width of the repeating biome
+create table inputs(line text);
+
+.mode csv
+.import 03_input.txt inputs
+
+-- Preprocess input into table of tree coordinates
+create temporary table coordinates as
+    select
+        inputs.rowid - 1 x, split.value square
+    from
+        inputs, split(inputs.line,"");
+
+create temporary table trees as
+    select
+        x, (rowid-1) % 31 y
+    from
+        coordinates
+    where
+        square = "#";
 
 -- Part 1
+
 select count(*) from trees where y = (3 * x) % 31;
 
-
 -- Part 2
-
 select
-  (select count(*) from trees where y = x % 31) *
-  (select count(*) from trees where y = (3 * x) % 31) *
-  (select count(*) from trees where y = (5 * x) % 31) *
-  (select count(*) from trees where y = (7 * x) % 31) *
-  (select count(*) from trees where x % 2 = 0 and y = (x/2) % 31);
+    count(*) filter (where y = x % 31) *
+    count(*) filter (where y = (3 * x) % 31) *
+    count(*) filter (where y = (5 * x) % 31) *
+    count(*) filter (where y = (7 * x) % 31) *
+    count(*) filter (where x % 2 = 0 and y = (x/2) % 31)
+from
+    trees;
